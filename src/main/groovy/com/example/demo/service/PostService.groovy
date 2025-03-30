31de6e782b9b8598947300b1b0409d66bed944ab
lenadekart <decartfamily@vk.com>
2025-03-30 17:36:24 +0500

    Created page for post creating and a corresponding controller.

--- src/main/groovy/com/example/demo/service/PostService.groovy
+++ src/main/groovy/com/example/demo/service/PostService.groovy
@@ -5,11 +5,20 @@ import org.springframework.stereotype.Service
 
 @Service
 class PostService {
-    public ArrayList<Post> listAllPosts() {
-        ArrayList<Post> posts = new ArrayList<>();
+    ArrayList<Post> posts;
+
+    public PostService(){
+        posts = new ArrayList<>();
         for (int i=1; i<=3; i++) {
             posts.add(new Post(new Date(), "Пост №"+i));
         }
+    }
+
+    public ArrayList<Post> listAllPosts() {
         return posts;
+    }
+
+    public void create(String text) {
+        posts.add(new Post(new Date(), text));
     }
 }
